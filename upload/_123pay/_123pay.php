990d953bd53de344db716da4558fa2373675678a
Hojjat Ali Mohammadi <safeith@riseup.net>
2017-07-09 14:24:12 +0430

    Update callback_url

---
+++ upload/_123pay/_123pay.php
@@ -0,0 +1,40 @@
+<?php
+
+session_start();
+if ( isset( $_SESSION['_123pay_merchant_id'] ) && isset( $_POST['_123pay_callback_url'] ) ) {
+	$item = $_POST['_123pay_item'];
+	if ( isset( $_SESSION[ '_123pay_amount' . $item ] ) ) {
+		echo 'Loading...';
+		function create( $merchant_id, $amount, $callback_url ) {
+			$ch = curl_init();
+			curl_setopt( $ch, CURLOPT_URL, 'https://123pay.ir/api/v1/create/payment' );
+			curl_setopt( $ch, CURLOPT_POSTFIELDS, "merchant_id=$merchant_id&amount=$amount&callback_url=$callback_url" );
+			curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+			curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+			$response = curl_exec( $ch );
+			curl_close( $ch );
+
+			return $response;
+		}
+
+		$merchant_id  = $_SESSION['_123pay_merchant_id'];
+		$amount       = $_SESSION[ '_123pay_amount' . $item ] * 10000;
+		$callback_url = urlencode( $_POST['_123pay_callback_url'] . $item );
+
+		$response = create( $merchant_id, $amount, $callback_url );
+		$result   = json_decode( $response );
+		if ( $result->status ) {
+			echo '<script>window.location=("' . $result->payment_url . '");</script>';
+			exit();
+		} else {
+			echo $result->message;
+			exit();
+		}
+	} else {
+		echo 'Plase Enter Valid Amount';
+		exit();
+	}
+} else {
+	echo 'Full Error';
+	exit();
+}
--- upload/includes/paymentapi/class__123pay.php
+++ upload/includes/paymentapi/class__123pay.php
@@ -7,86 +7,79 @@ class vB_PaidSubscriptionMethod__123pay extends vB_PaidSubscriptionMethod {
 	var $supports_recurring = false;
 	var $display_feedback = true;
 
-	function create( $merchant_id, $amount, $callback_url ) {
-		$ch = curl_init();
-		curl_setopt( $ch, CURLOPT_URL, 'https://123pay.ir/api/v1/create/payment' );
-		curl_setopt( $ch, CURLOPT_POSTFIELDS, "merchant_id=$merchant_id&amount=$amount&callback_url=$callback_url" );
-		curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
-		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-		$response = curl_exec( $ch );
-		curl_close( $ch );
-
-		return $response;
-	}
-
 	function verify_payment() {
-		$this->registry->input->clean_array_gpc( 'r', array(
-			'item'   => TYPE_STR,
-			'State'  => TYPE_STR,
-			'RefNum' => TYPE_STR
-		) );
-
-		$this->State          = $_REQUEST['State'];
-		$this->transaction_id = $_REQUEST['RefNum'];
-
-		if ( ! empty( $this->registry->GPC['item'] ) && ! empty( $this->transaction_id ) ) {
-			$this->paymentinfo = $this->registry->db->query_first( "
+		@session_start();
+		if ( isset( $_SESSION['_123pay_merchant_id'] ) && ( isset( $_REQUEST['State'] ) && $_REQUEST['State'] == 'OK' ) && isset( $_REQUEST['RefNum'] ) ) {
+			$merchant_id = $_SESSION['_123pay_merchant_id'];
+			$this->registry->input->clean_array_gpc( 'r', array( 'item' => TYPE_STR, 'au' => TYPE_STR ) );
+			$this->transaction_id = $this->registry->GPC['item'];
+			if ( ! empty( $this->registry->GPC['item'] ) ) {
+				$this->paymentinfo = $this->registry->db->query_first( "
 				SELECT paymentinfo.*, user.username
 				FROM " . TABLE_PREFIX . "paymentinfo AS paymentinfo
 				INNER JOIN " . TABLE_PREFIX . "user AS user USING (userid)
 				WHERE hash = '" . $this->registry->db->escape_string( $this->registry->GPC['item'] ) . "'
 			" );
-			if ( ! empty( $this->paymentinfo ) ) {
-				$sub = $this->registry->db->query_first( "SELECT * FROM " . TABLE_PREFIX . "subscription WHERE subscriptionid = " . $this->paymentinfo['subscriptionid'] );
+				if ( ! empty( $this->paymentinfo ) ) {
+					$sub    = $this->registry->db->query_first( "SELECT * FROM " . TABLE_PREFIX . "subscription WHERE subscriptionid = " . $this->paymentinfo['subscriptionid'] );
+					$cost   = unserialize( $sub['cost'] );
+					$amount = floor( $cost[0][ cost ][ usd ] );
+					$amount = $amount * 10000;
+					$RefNum = $_REQUEST['RefNum'];
+					$ch     = curl_init();
+					curl_setopt( $ch, CURLOPT_URL, 'https://123pay.ir/api/v1/verify/payment' );
+					curl_setopt( $ch, CURLOPT_POSTFIELDS, "merchant_id=$merchant_id&RefNum=$RefNum" );
+					curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+					curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+					$response = curl_exec( $ch );
+					curl_close( $ch );
+					
+					$result = json_decode( $response );
+					if ( $result->status ) {
+						$this->paymentinfo['currency'] = 'usd';
+						$this->paymentinfo['amount']   = $cost[0][ cost ][ usd ];
+						$this->type                    = 1;
 
-				$cost   = unserialize( $sub['cost'] );
-				$amount = floor( $cost[0][ cost ][ usd ] * $this->settings['d2t'] ) * 10;
+						return true;
+					} else {
+						$this->error = 'Failed Transaction !';
 
-
-				$State  = $_REQUEST['State'];
-				$RefNum = $_REQUEST['RefNum'];
-
-				$response = $this->verify( $this->settings['plmid'], $RefNum );
-				$result   = json_decode( $response );
-
-				if ( $State == 'OK' && $result->status && $amount == $result->amount && $amount == $_REQUEST['amount_check'] ) {
-					$this->paymentinfo['currency'] = 'usd';
-					$this->paymentinfo['amount']   = $cost[0][ cost ][ usd ];
-					$this->type                    = 1;
-
-					return true;
+						return false;
+					}
 				}
 			}
-		}
-		$this->error = 'Duplicate transaction.';
 
-		return false;
+		} else {
+			$this->error = 'Error !';
+
+			return false;
+		}
+
 	}
 
+	function test() {
+		@session_start();
+
+		return true;
+	}
 
 	function generate_form_html( $hash, $cost, $currency, $subinfo, $userinfo, $timeinfo ) {
-		global $vbphrase, $vbulletin, $show;
-
 		$item = $hash;
-		$cost = ( floor( $cost * $this->settings['d2t'] ) ) * 10;
-		$api  = $this->settings['plmid'];
-
-		$form['action'] = '_123pay.php';
-		$form['method'] = 'POST';
-
-		$settings =& $this->settings;
-
-		$templater = vB_Template::create( 'subscription_payment__123pay' );
-		$templater->register( 'API', $api );
-		$templater->register( 'cost', $cost );
+		global $vbphrase, $vbulletin, $show;
+		@session_start();
+		$cost                                 = floor( $cost );
+		$_SESSION[ '_123pay_amount' . $item ] = $cost;
+		$form['action']                       = '_123pay/_123pay.php';
+		$form['method']                       = 'POST';
+		$settings                             = &$this->settings;
+		$templater                            = vB_Template::create( '_123pay' );
 		$templater->register( 'item', $item );
-		$templater->register( 'subinfo', $subinfo );
-		$templater->register( 'settings', $settings );
 		$templater->register( 'userinfo', $userinfo );
 		$form['hiddenfields'] .= $templater->render();
 
 		return $form;
 	}
+
 }
 
 ?>
